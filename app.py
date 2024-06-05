c1f9512c07e07804a0d77b8c2cc81e60f9810c53
Prakashmaheshwaran <zskillhelps@gmail.com>
2024-06-05 23:39:36 +0530

    codespace test

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
-from flask import Flask, request, jsonify
-import requests
-import os
+from flask import Flask, jsonify
+import platform
+import psutil
 
 app = Flask(__name__)
 
-# Access environment variables directly
-UNSPLASH_ACCESS_KEY = os.getenv("UNSPLASH_ACCESS_KEY")
-
-@app.route('/get-image', methods=['GET'])
-def get_image():
-    keyword = request.args.get('keyword')
-    if not keyword:
-        return jsonify({'error': 'Keyword is required'}), 400
-
-    unsplash_search_url = 'https://api.unsplash.com/search/photos'
-    params = {
-        'query': keyword,
-        'per_page': 1,
-        'orientation': 'landscape',
-        'client_id': UNSPLASH_ACCESS_KEY
+@app.route('/specs', methods=['GET'])
+def get_specs():
+    specs = {
+        "CPU": platform.processor(),
+        "Memory": f"{psutil.virtual_memory().total / (1024.0 **3):.2f} GB",
+        "Storage": f"{psutil.disk_usage('/').total / (1024.0 **3):.2f} GB"
     }
-    response = requests.get(unsplash_search_url, params=params)
-    if response.status_code == 200:
-        image_data = response.json()
-        if image_data['results']:
-            image_url = image_data['results'][0]['urls']['regular']
-            return jsonify({'image_url': image_url})
-        else:
-            return jsonify({'error': 'No images found'}), 404
-    else:
-        return jsonify({'error': 'Failed to fetch images'}), response.status_code
+    return jsonify(specs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
