2922b51450ed7b655ca727f96ca6c1f43691051e
Prakashmaheshwaran <zskillhelps@gmail.com>
2024-06-06 21:30:17 +0530

    cpu GHz

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ def home():
 
 @app.route('/specs', methods=['GET'])
 def get_specs():
+    cpu_freq = psutil.cpu_freq()
     specs = {
         "CPU": platform.processor(),
         "Memory": f"{psutil.virtual_memory().total / (1024.0 **3):.2f} GB",
-        "Storage": f"{psutil.disk_usage('/').total / (1024.0 **3):.2f} GB"
+        "Storage": f"{psutil.disk_usage('/').total / (1024.0 **3):.2f} GB",
+        "CPU Frequency": f"{cpu_freq.current / 1000:.2f} GHz"
     }
     return jsonify(specs)
 
