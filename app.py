aeddf8cdb0fedab5417eabd709f4c8b002622832
Prakashmaheshwaran <zskillhelps@gmail.com>
2024-06-06 20:18:37 +0530

    new 1

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-from flask import Flask, render_template_string
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # URL to be opened in iframe
-    url = "https://server.duinocoin.com/"
-    # HTML content with iframe
-    html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Preview</title>
-    </head>
-    <body>
-        <iframe src="{url}" width="100%" height="100%"></iframe>
-    </body>
-    </html>
-    """
-    return render_template_string(html_content)
+UNSPLASH_ACCESS_KEY = 'YOUR_UNSPLASH_ACCESS_KEY'  # Replace this with your Unsplash API Access Key
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/')
+def home():
+    return "This is an API page"
+
+@app.route('/image-search', methods=['GET'])
+def image_search():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({"error": "No query parameter provided"}), 400
+
+    url = "https://api.unsplash.com/search/photos"
+    params = {
+        'query': query,
+        'client_id': UNSPLASH_ACCESS_KEY,
+        'per_page': 10
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        return jsonify({"error": "Failed to fetch images from Unsplash"}), response.status_code
+
+    data = response.json()
+    images = [{'id': img['id'], 'url': img['urls']['regular'], 'description': img['description']} for img in data['results']]
+
+    return jsonify(images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
