78fd73a728a53a8644f8311947a7c49bd2a3643b
PrakashMahesh2729 <zskillhelps@gmail.com>
2024-06-21 21:51:29 +0530

    added YT Transcript

--- addons/check_work.py
+++ addons/check_work.py
@@ -11,7 +11,7 @@ def get_specs():
         "CPU": platform.processor(),
         "Memory": f"{psutil.virtual_memory().total / (1024.0 ** 3):.2f} GB",
         "Storage": f"{psutil.disk_usage('/').total / (1024.0 ** 3):.2f} GB",
-        "CPU Frequency": f"{cpu_freq.current / 1000:.2f} GHz"
+        # "CPU Frequency": f"{cpu_freq.current / 1000:.2f} GHz"
     }
     return jsonify(specs)
 
