bec5b283b07bb917b55ddde2fafcd5978f110b60
PrakashMahesh2729 <zskillhelps@gmail.com>
2024-06-21 22:34:29 +0530

    removed YT-transcript

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, send_file
 from addons.check_work import get_specs, generate_video
 from addons.fetchpost import get_published_and_scheduled_posts
 from addons.image_upload import fetch_and_upload_image
-from youtube_transcript_api import YouTubeTranscriptApi
 
 
 app = Flask(__name__)
@@ -57,18 +56,6 @@ def fetch_and_upload_image_endpoint():
 def edit_video():
     output_path = generate_video()
     return send_file(output_path, as_attachment=True, mimetype='video/mp4')
-
-@app.route('/yt-transcript', methods=['GET'])
-def get_youtube_transcript():
-    video_id = request.args.get('id')
-    if not video_id:
-        return jsonify({"error": "Video ID is required"}), 400
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-        transcript_text = ' '.join([entry['text'] for entry in transcript])
-        return jsonify({"transcript": transcript_text})
-    except Exception as e:
-        return jsonify({"error": "No transcript available"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
